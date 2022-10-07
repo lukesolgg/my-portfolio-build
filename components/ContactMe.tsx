035c0312662867ba9e8c3fdedbc6d57c74ce72f2
lukesolgg <lukesolgg@gmail.com>
2022-10-07 10:54:07 +0100

    modified globals css and contact page

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -19,15 +19,15 @@ function ContactMe({}: Props) {
   };
   return (
     <div
-      className="h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-5 
-    justify-evenly mx-auto items-center"
+      className="h-screen flex relative flex-col text-center md:text-left max-w-7xl px-10 pt-0 
+      place-content-center gap-y-10 mx-auto items-center"
     >
       <h3 className="absolute top-12  uppercase tracking-[1rem] text-gray-500 text-2xl md:top-20">
         Contact
       </h3>
 
-      <div className="flex flex-col space-y-10 px-7 sm:px-2">
-        <h4 className="text-2xl md:text-4xl font-semibold text-center">
+      <div className="flex flex-col space-y-10">
+        <h4 className="text-xl md:text-3xl font-semibold text-center">
           Reach out for Business Propositions.{ ' ' }
           <span className="underline decoration-[#2BAAE2]/50">Lets Talk</span>
         </h4>
@@ -35,21 +35,21 @@ function ContactMe({}: Props) {
         <div>
           <div className="flex items-center space-x-5 justify-center">
             <PhoneIcon className="text-[#2BAAE2] h-7 w-7 animate-pulse" />
-            <p className="text-xl">+44769333333</p>
+            <p className="text-xs md:text-xl">+44769333333</p>
           </div>
           <div className="flex items-center space-x-5 justify-center">
             <EnvelopeIcon className="text-[#2BAAE2] h-7 w-7 animate-pulse" />
-            <p className="text-xl">lukeknightofficial@gmail.com</p>
+            <p className="text-xs md:text-xl">lukeknightofficial@gmail.com</p>
           </div>
           <div className="flex items-center space-x-5 justify-center">
             <MapPinIcon className="text-[#2BAAE2] h-7 w-7 animate-pulse" />
-            <p className="text-xl">123 Developer Lane</p>
+            <p className="text-xs md:text-xl">123 Developer Lane</p>
           </div>
         </div>
 
         <form
           onSubmit={handleSubmit(onSubmit)}
-          className="flex flex-col space-y-2 w-fit mx-auto p-2"
+          className="flex flex-col space-y-2 w-fit mx-auto"
         >
           <div className="flex space-x-2">
             <input
